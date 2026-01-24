568cce78d5f16bae3e7b52b4b2433b1e777214ce
ByteByChandan <iamchandan1110@gmail.com>
2026-01-24 23:57:51 +0530

    add 2 prgm

---
+++ OOP/Inheritance.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Inheritance{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("hello world");
+        sc.close();
+    }
+}
---
+++ OOP/Method_Overloading.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Overloading{
+ public int sum(int a,int b){
+    int sum=a+b;
+    return sum;
+ }
+ public int sum(int a ,int b,int c){
+    int sum =a+b+c;
+    return sum;
+ }
+}
+public class Method_Overloading {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your 1st digit: ");
+            int a=sc.nextInt();
+        System.out.println("Enter your 2nd digit: ");
+            int b=sc.nextInt();
+        System.out.println("Enter your 3rd digit: ");
+            int c=sc.nextInt();
+        Overloading obj=new Overloading();
+        int Two_sum=obj.sum(a, b);
+        System.out.println("Your 2 digit sum is: "+ Two_sum);
+        int Three_sum=obj.sum(a, b, c);
+        System.out.println("Your 3 digit sum is: "+ Three_sum);
+    }
+}
