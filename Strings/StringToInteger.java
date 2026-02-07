12d0e66f93a3b2a28905033b10e0590e9f41d150
ByteByChandan <iamchandan1110@gmail.com>
2026-02-07 23:58:37 +0530

    add StringToInteger.java

--- Strings/StringToInteger.java
+++ Strings/StringToInteger.java
@@ -1,28 +1,41 @@
 import java.util.*;
+
 public class StringToInteger {
+
     public static int myAtoi(String s) {
         s = s.trim();
         if (s.isEmpty()) return 0;
 
         int sign = 1, i = 0, n = s.length();
         long num = 0;
+
         if (s.charAt(0) == '-' || s.charAt(0) == '+') {
             sign = (s.charAt(0) == '-') ? -1 : 1;
             i++;
         }
+
         while (i < n && Character.isDigit(s.charAt(i))) {
             num = num * 10 + (s.charAt(i) - '0');
-            if (sign * num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            if (sign * num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
+            if (sign * num < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+
+            if (sign * num > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+
             i++;
         }
         return (int) (sign * num);
     }
-    public static void main(String []agrs){
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter your String: ");
-        String s=sc.nextLine();
-        StringBuffer str=new StringBuffer(s);
-        myAtoi(s);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your String: ");
+        String s = sc.nextLine();
+
+        int result = myAtoi(s);
+        System.out.println("Converted Integer: " + result);
+
+        sc.close();
     }
 }
