a23cef3b8915aeccd78dc5c7b358b17ea102d209
ByteByChandan <iamchandan1110@gmail.com>
2026-02-20 00:57:27 +0530

    add Find_factorial.java

--- hasing/practice.java
+++ hasing/practice.java
@@ -1,5 +1,26 @@
-public class practice {for(HashMap.Entry<String, Integer> entry : map.entrySet()) {
-    System.out.println(entry.getKey() + " " + entry.getValue());
-}
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String []args){
+        Scanner sc =new Scanner(System.in);
+        HashMap<String,Integer> mp=new HashMap<>();
+        mp.put("Chandan",98);
+        mp.put("Sayan",87);
+        mp.put("Rahul",88);
+//        System.out.print("enter your range: ");
+//        int n=sc.nextInt();
+//        for(int i=0;i<n;i++){
+//            System.out.println("enter your name: ");
+//            String name=sc.nextLine();
+//            System.out.println("enter your roll number: ");
+//            int roll=sc.nextInt();
+//            sc.nextLine();
+//            mp.put(name,roll);
+//
+//        }
+        System.out.println(mp.get("Chandan"));
+        System.out.println(mp.get("ram"));// return null
+    }
 
 }
