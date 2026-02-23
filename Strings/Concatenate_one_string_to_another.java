701fafe75b8b5ae3ba62633dcfa108c2d43de015
ByteByChandan <iamchandan1110@gmail.com>
2026-02-23 22:59:43 +0530

    add array_marge not complete

--- Strings/Concatenate_one_string_to_another.java
+++ Strings/Concatenate_one_string_to_another.java
@@ -1,11 +1,20 @@
+import java.util.*;
 public class Concatenate_one_string_to_another {
         public static void main(String[] args) {
-            String str1 = "Hello";
-            String str2 = " World";
+//            String str1 = "Hello";
+//            String str2 = " World";
+//
+//            String result = str1.concat(str2);
+//
+//            System.out.println(result);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your First string: ");
+            String str1=sc.nextLine();
+            System.out.println("Enter your Sceond String: ");
+            String str2=sc.nextLine();
 
-            String result = str1.concat(str2);
-
-            System.out.println(result);
+            String new_str=str1.concat(str2);
+            System.out.println(" After the concatination the new String is: "+new_str);
         }
     }
 
