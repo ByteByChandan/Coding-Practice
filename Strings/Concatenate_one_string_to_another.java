d5142736e1490d42600ecc8afd5f1eca54db57e8
ByteByChandan <iamchandan1110@gmail.com>
2026-02-23 22:54:44 +0530

    add array_marge not complete

---
+++ Strings/Concatenate_one_string_to_another.java
@@ -0,0 +1,11 @@
+public class Concatenate_one_string_to_another {
+        public static void main(String[] args) {
+            String str1 = "Hello";
+            String str2 = " World";
+
+            String result = str1.concat(str2);
+
+            System.out.println(result);
+        }
+    }
+
