9ace23bc84af6d4c3714a6c19f465007d963312b
ByteByChandan <iamchandan1110@gmail.com>
2026-02-24 23:08:09 +0530

    add array_marge not complete

---
+++ arrays/Taking_input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Taking_input {
+    public static void main(String []args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter your n:");
+        int n=sc.nextInt();
+        ArrayList<Integer>arr=new ArrayList<>(n);
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+    }
+}
