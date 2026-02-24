5dbdc0edfb58a5bf89091e97868002923770ac78
ByteByChandan <iamchandan1110@gmail.com>
2026-02-25 00:51:58 +0530

    add array_marge not complete

--- arrays/Taking_input.java
+++ arrays/Taking_input.java
@@ -9,5 +9,11 @@ public class Taking_input {
         arr.add(1);
         arr.add(2);
         arr.add(3);
+        for(int i=0;i<5;i++){
+            arr.add(sc.nextInt());
+        }
+        for(int i=0;i<arr.size();i++){
+            System.out.println(arr.get(i));
+        }
     }
 }
