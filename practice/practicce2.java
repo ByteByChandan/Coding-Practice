7aa3d0841c7823592f0fd72191ed3b9cd746b71c
ByteByChandan <iamchandan1110@gmail.com>
2026-02-28 23:16:27 +0530

    add array_marge not complete

--- practice/practicce2.java
+++ practice/practicce2.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class practicce2 {
     public static void main(String []args){
-
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter your array Seprated by coma: ");
+        String s=sc.nextLine();
+        String [] input=s.split(",");
+        int []array=new int[input.length];
+        for(int i =0;i<array.length;i++){
+            array[i]=Integer.parseInt(input[i].trim());
+        }
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]);
+        }
     }
 }
