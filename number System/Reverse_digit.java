98aba42e9adae23ad52855df7519ba4558e03e1b
ByteByChandan <iamchandan1110@gmail.com>
2026-03-10 02:11:22 +0530

    add array_marge not complete

--- number System/Reverse_digit.java
+++ number System/Reverse_digit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class  rev{
-    public int number_reverse (int num){
+    public String number_reverse (int num){
         int n=num;
         int temp=0;
         while (n!=0) {
@@ -8,7 +8,8 @@ class  rev{
             temp=temp*10+digit;
             n=n/10;
         }
-        return temp;
+        String temm=Integer.toString(temp);
+        return temm;
         
     }
 }
@@ -18,7 +19,7 @@ class reverse_digit {
         System.out.println("Please enter your Number: ");
         int number=sc.nextInt();
         rev obj=new rev();
-        int reverse=obj.number_reverse(number);
+        String reverse=obj.number_reverse(number);
         System.out.println("The reverse Number is: "+reverse);
         sc.close();
     }
