94b11f8d6010ac2d4ac91be88a09a21cdd505eb8
ByteByChandan <iamchandan1110@gmail.com>
2026-03-11 02:18:23 +0530

    add array_marge not complete

---
+++ practice/float_sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class float_sum{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String s2=sc.nextLine();
+        Float a= Float.parseFloat(s);
+        Float b=Float.parseFloat(s2);
+        Float sum=a+b;
+        System.out.print(sum);
+
+
+    }
+}
