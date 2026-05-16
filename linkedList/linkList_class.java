70071a83aaa7c35e49b4e6394db00b2c24134400
ByteByChandan <iamchandan1110@gmail.com>
2026-05-16 11:57:53 +0530

    hasmap practice 2

---
+++ linkedList/linkList_class.java
@@ -0,0 +1,53 @@
+public class linkList_class{
+    public static class node{
+        int data;
+        node next;
+        node(int data){
+            this.data=data;
+        }
+
+    }
+    public static class linkedlist{
+        node head=null;
+        node tail=null;
+        void insertAtEnd(int val){
+            node temp=new node(val);
+            if(head==null){
+                head=temp;
+                tail=temp;
+            }
+            else{
+                tail.next=temp;
+                tail=temp;
+            }
+        }
+        void display(){
+            node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+        }
+        int size(){
+            node temp=head;
+            int count=0;
+            while(temp!=null){
+                count++;
+                temp=temp.next;
+            }
+            return count;
+        }
+
+    }
+    public static void main(String []args){
+        linkedlist ll=new linkedlist();
+        ll.insertAtEnd(3);
+        ll.insertAtEnd(4);
+        ll.insertAtEnd(76);
+        ll.insertAtEnd(76);
+        ll.display();
+        System.out.println();
+        int size=ll.size();
+        System.out.println(size);
+    }
+}
