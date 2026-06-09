1ea4e4ca0e00d65616bdccb6b92215578e800778
ByteByChandan <iamchandan1110@gmail.com>
2026-06-09 12:01:28 +0530

    hasmap practice 2

--- practice/Linkedlist_practice/linkedlist_practice2.java
+++ practice/Linkedlist_practice/linkedlist_practice2.java
@@ -51,6 +51,25 @@ public class linkedlist_practice2 {
                 head=temp;
             }
         }
+        void insert(int value,int idx){
+            node temp = new node (value);
+            node t=head;
+            for(int i =1;i<=idx-1;i++){
+                t=t.next;
+            }
+            temp.next=t.next;
+            t.next=temp;
+            if(idx==0){
+                insertAtbegining(value);
+            }
+             else if (idx<0|| idx>size()) {
+            System.out.print("Invaild index");
+        }
+             else if(idx==size()){
+                instert(value);
+            }
+
+        }
 
     }
     public static void main(String []args){
@@ -62,6 +81,8 @@ public class linkedlist_practice2 {
         int size=ll.size();
         System.out.println("your size of a linkedlist is: "+size);
         ll.insertAtbegining(0);
+        ll.display();
+        ll.insert(54,2);
         ll.display();
     }
 }
