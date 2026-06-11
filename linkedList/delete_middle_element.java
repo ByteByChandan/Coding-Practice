69ad672e077f51deb2845b5bbef15193467076fe
ByteByChandan <iamchandan1110@gmail.com>
2026-06-11 13:56:56 +0530

    hasmap practice 2

--- linkedList/delete_middle_element.java
+++ linkedList/delete_middle_element.java
@@ -1,4 +1,3 @@
-
 public class delete_middle_element {
     public static class node{
         int data;
@@ -8,60 +7,48 @@ public class delete_middle_element {
         }
     }
     public static class linkedlist{
-        node head= null;
+        node head=null;
         node tail=null;
         void add(int data){
-            node temp= new node(data);
+            node temp=new node(data);
             if(head==null){
-                head= temp;
+                head=temp;
             }
-            else {
+            else{
                 tail.next=temp;
             }
             tail=temp;
         }
         void display(){
             node temp=head;
-             while(temp!=null){
-                 System.out.print(temp.data+" ");
-                 temp=temp.next;
-             }
-             System.out.println();
-        }
-
-        void leftMiddle(){
-            node fast=head.next;
-            node slow=head;
-            while(fast!=null&& fast.next!=null){
-                slow=slow.next;
-                fast=fast.next.next;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
             }
-            System.out.print("Left Middle of the element is : "+ slow.data);
             System.out.println();
-        }
 
-        void rightMiddle(){
-            node slow =head;
-            node fast=head;
-            while(fast!=null&& fast.next!=null){
+        }
+        void deleteMid(){
+            if(head.next==null) return;
+            node fast= head.next;
+            node slow= head;
+            while(fast.next.next!=null&& fast.next.next.next!=null){
                 slow=slow.next;
                 fast=fast.next.next;
             }
-            System.out.print("Right Middle of the element is : "+ slow.data);
-
+            slow.next=slow.next.next;
+            display();
         }
     }
-    public static void main(String []args){
+    public static void main(String[] args){
         linkedlist ll= new linkedlist();
-        ll.add(56);
-        ll.add(67);
+        ll.add(6);
+        ll.add(7);
         ll.add(12);
-        ll.add(76);
-        ll.add(89);
-        ll.add(100);
+        ll.add(1);
+        ll.add(8);
+        ll.add(10);
         ll.display();
-        ll.leftMiddle();
-        ll.rightMiddle();
-
+        ll.deleteMid();
     }
 }
